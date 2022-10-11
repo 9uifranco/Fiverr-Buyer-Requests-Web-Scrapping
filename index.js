b9f196059a55f4a48e792f78ddab875c2b8efc25
Guilherme Franco <9uifranco@gmail.com>
2022-10-11 19:17:51 -0300

    Grab requests from Buyer Requests first page and send via Telegram Bot to user

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const puppeteer = require('puppeteer'); // "import" puppeteer
 // to look more like a user
 const puppeteerExtra = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+// telegram bot
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, {polling: true});
+
+bot.on('message', (msg) => {
+
+    var Hi = "hi";
+    if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id,"Hold on, I'm working rn");
+    }
+    
+});
 
 // most of the functions we use in puppeteer returns a promise, which makes sense 'cause when ur webscrapping
 // you basically telling a bot to wait something to happen to then take some action, and that's what ASYNC AWAIT does
@@ -17,38 +30,84 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
     await page.setViewport({ width: 1366, height: 768});
     await page.goto("https://www.fiverr.com/login?source=top_nav");
 
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 6000));
 
     await page.click(".icon-button.social-signing-button.google-signing-button");
-    await new Promise(r => setTimeout(r, 3000));
+    await new Promise(r => setTimeout(r, 4000));
 
     // Log in
 
     const pages = await browser.pages();
  
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 6000));
 
     await pages[2].keyboard.type(process.env.EMAIL, { delay: 50 });
     await pages[2].click('#identifierNext');
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 6000));
     await pages[2].keyboard.type(process.env.PASSWORD, { delay: 50 });
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 6000));
     await pages[2].click('#passwordNext');
 
     // you gonna need to allow on your device now, or disable google 2fa (recommended for this purpose)
 
-    await new Promise(r => setTimeout(r, 5000));
+    await new Promise(r => setTimeout(r, 6000));
 
     // After logged in, go to "Buyer Requests" page
     
     let buyerRequestsURL = "https://www.fiverr.com/users/" + process.env.FIVERR_USERNAME + "/requests?source=header_nav";
 
     await page.goto(buyerRequestsURL);
-    console.log(buyerRequestsURL);
     await new Promise(r => setTimeout(r, 10000));
     await page.screenshot({ path: "fiverr-screenshot-buyer-request.png" });
 
-    await browser.close();
+    // Grab all requests which needs to be opened
+
+    const grabRequests = await page.evaluate(() => {
+
+        const infos = document.querySelectorAll(".db-new-main-table.align-top.js-db-table table tbody tr .see-more .ellipsis.text-wrap span");
+
+        let listOfRequests = [];
+        infos.forEach((info) => {
+            listOfRequests.push(info.innerText)
+        });
+        
+        return listOfRequests;
+    });
+
+    // Click on every single "See More"
+
+    const gigs = await page.$$(".db-new-main-table.align-top.js-db-table table tbody tr .see-more .ellipsis.text-wrap span a");
+    
+    for (const requestIndex of grabRequests.keys()) {
+        if(gigs[requestIndex]) {
+            console.log(requestIndex);
+            await gigs[requestIndex].click();
+        }
+    }
+
+    // Grab all the requests, now opened
+
+    const grabAllRequests = await page.evaluate(() => {
+
+        const infos = document.querySelectorAll(".db-new-main-table.align-top.js-db-table table tbody tr td:nth-child(3) span");
+
+        let allRequests = [];
+        infos.forEach((info) => {
+            allRequests.push(info.innerText)
+        });
+        
+        return allRequests;
+    });
+
+    console.log(grabAllRequests);
+
+    // Send all requests from the first page
+
+    for (const requestIndex of grabAllRequests.keys()) {
+        bot.sendMessage(process.env.CHAT_ID,grabAllRequests[requestIndex].toString());
+    }
+
+    //await browser.close();
 })();
 
 /*
